b8406c75110cd425f108ffedd29cd7a9da56f2f5
Xzai <Xzai@DESKTOP-98F0SOC>
2020-04-25 10:00:42 +0800

    commit of Xzai

---
+++ src/main/java/learning/java/homework/h20200414/H3.java
@@ -0,0 +1,29 @@
+package learning.java.homework.h20200414;
+
+import java.util.ArrayList;
+
+public class H3 {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> h001 = new ArrayList<Integer>();
+        String i01 = "小王";
+        String i02 = "小美";
+        String i03 = "小刘";
+        String i04 = 67;
+        String i05 = 18;
+        String i06 = 14;
+        String i07 = 78;
+        String i08 = 55;
+        String i09 = 25;
+        h001.add(i01);
+        h001.add(i02);
+        h001.add(i03);
+        h001.add(i04);
+        h001.add(i05);
+        h001.add(i06);
+        h001.add(i07);
+        h001.add(i08);
+        h001.add(i09);
+	}
+
+}
