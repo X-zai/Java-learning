ecbd254f5a0c3eb3e138a49b77f352483f3ca67e
Xzai <Xzai@DESKTOP-98F0SOC>
2020-04-28 19:57:29 +0800

    commit with Xzai

--- src/main/java/learning/java/homework/h20200421/H3.java
+++ src/main/java/learning/java/homework/h20200421/H3.java
@@ -3,18 +3,17 @@ package learning.java.homework.h20200421;
 public class H3 {
 
 	public static void main(String[] args) {
-		char[] ch = null;
-		char[] cha = str(ch);
+		String ch = null;
+		String str = "123 abc 编程";
+		char[] cha = str(str);
 		for (int i = 0; i < cha.length; i++) {
 			System.out.println(cha[i]);
 		}
 	}
 
-	public static char[] str(char[] ch) {
-		String str = "123";
-		for (int i = 0; i < str.length(); i++) {
-			ch = str.toCharArray();
-		}
+	public static char[] str(String str) {
+		char[] ch = null;
+		ch = str.toCharArray();
 		return ch;
 	}
 }
