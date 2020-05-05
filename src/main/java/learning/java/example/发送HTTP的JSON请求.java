bd50c58a3368874ddad805a150b2a6684a4f2544
Xzai <Xzai@DESKTOP-98F0SOC>
2020-05-05 10:49:43 +0800

    commit with Xzai

--- src/main/java/learning/java/example/发送HTTP的JSON请求.java
+++ src/main/java/learning/java/example/发送HTTP的JSON请求.java
@@ -20,19 +20,17 @@ public class 发送HTTP的JSON请求 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		while (true) {
-			try {
-				String json = "{\n" + "  \"msgtype\": \"text\",\n" + "  \"text\": {\n" + "    \"content\": \"java\"\n"
-						+ "  },\n" + "  \"at\": {\n" + "    \"atMobiles\": [\n" + "      \"156xxxx8827\",\n"
-						+ "      \"189xxxx8325\"\n" + "    ],\n" + "    \"isAtAll\": false\n" + "  }\n" + "}";
-				Request.Post(
-						"https://oapi.dingtalk.com/robot/send?access_token=f5896bed709418fcaa5da249b63de95db4c1a7c8c7bdee9750e668791b7308e4")
-						.useExpectContinue().version(HttpVersion.HTTP_1_1)
-						.bodyString(json, ContentType.APPLICATION_JSON).execute().returnContent().asBytes();
-				Thread.sleep(1 * 1000 * 60 * 10);
-			} catch (Exception e) {
-				// TODO: handle exception
-			}
+		try {
+			String json = "{\n" + "  \"msgtype\": \"text\",\n" + "  \"text\": {\n"
+					+ "    \"content\": \"java\"\n" + "  },\n" + "  \"at\": {\n"
+					+ "    \"atMobiles\": [\n" + "      \"156xxxx8827\",\n" + "      \"189xxxx8325\"\n" + "    ],\n"
+					+ "    \"isAtAll\": false\n" + "  }\n" + "}";
+			Request.Post(
+					"https://oapi.dingtalk.com/robot/send?access_token=f5896bed709418fcaa5da249b63de95db4c1a7c8c7bdee9750e668791b7308e4")
+					.useExpectContinue().version(HttpVersion.HTTP_1_1).bodyString(json, ContentType.APPLICATION_JSON)
+					.execute().returnContent().asBytes();
+		} catch (Exception e) {
+			// TODO: handle exception
 		}
 	}
 }
