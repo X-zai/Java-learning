122f6f7eed030d8b986e70eee13b0f193db5799c
Rayliu <rayliu10k@gmail.com>
2020-08-05 14:28:34 +0800

    修复编译错误

--- src/main/java/learning/java/practice/p20200614/Wc1.java
+++ src/main/java/learning/java/practice/p20200614/Wc1.java
@@ -7,70 +7,70 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Wc1 {
-	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-		Map<String, Wc_cs> wc = new HashMap<String, Wc_cs>();
-		Wc_cs w1 = new Wc_cs(1, "1930", "乌拉圭", "乌拉圭");
-        Wc_cs w2 = new Wc_cs(2, "1934", "意大利", "意大利");
-        Wc_cs w3 = new Wc_cs(3, "1938", "法国", "意大利");
-        Wc_cs w4 = new Wc_cs(4, "1950", "巴西", "乌拉圭");
-        Wc_cs w5 = new Wc_cs(5, "1954", "瑞士", "西德");
-        Wc_cs w6 = new Wc_cs(6, "1958", "瑞典", "巴西");
-        Wc_cs w7 = new Wc_cs(7, "1962", "智利", "巴西");
-        Wc_cs w8 = new Wc_cs(8, "1966", "英格兰", "英格兰");
-        Wc_cs w9 = new Wc_cs(9, "1970", "墨西哥", "巴西");
-        Wc_cs w10 = new Wc_cs(10, "1974", "前西德", "西德");
-        Wc_cs w11 = new Wc_cs(11, "1978", "阿根廷", "阿根廷");
-        Wc_cs w12 = new Wc_cs(12, "1982", "西班牙", "意大利");
-        Wc_cs w13 = new Wc_cs(13, "1986", "墨西哥", "阿根廷");
-        Wc_cs w14 = new Wc_cs(14, "1990", "意大利", "西德");
-        Wc_cs w15 = new Wc_cs(15, "1994", "美国", "巴西");
-        Wc_cs w16 = new Wc_cs(16, "1998", "法国", "法国");
-        Wc_cs w17 = new Wc_cs(17, "2002", "韩日", "巴西");
-        Wc_cs w18 = new Wc_cs(18, "2006", "德国", "意大利");
-        Wc_cs w19 = new Wc_cs(19, "2010", "南非", "西班牙");
-        Wc_cs w20 = new Wc_cs(20, "2014", "巴西", "德国");
-        Wc_cs w21 = new Wc_cs(21, "2018", "俄罗斯", "法国");
-		wc.put(w1.jvBanNianShu, w1);
-		wc.put(w2.jvBanNianShu, w2);
-		wc.put(w3.jvBanNianShu, w3);
-		wc.put(w4.jvBanNianShu, w4);
-		wc.put(w5.jvBanNianShu, w5);
-		wc.put(w6.jvBanNianShu, w6);
-		wc.put(w7.jvBanNianShu, w7);
-		wc.put(w8.jvBanNianShu, w8);
-		wc.put(w9.jvBanNianShu, w9);
-		wc.put(w10.jvBanNianShu, w10);
-		wc.put(w11.jvBanNianShu, w11);
-		wc.put(w12.jvBanNianShu, w12);
-		wc.put(w13.jvBanNianShu, w13);
-		wc.put(w14.jvBanNianShu, w14);
-		wc.put(w15.jvBanNianShu, w15);
-		wc.put(w16.jvBanNianShu, w16);
-		wc.put(w17.jvBanNianShu, w17);
-		wc.put(w18.jvBanNianShu, w18);
-		wc.put(w19.jvBanNianShu, w19);
-		wc.put(w20.jvBanNianShu, w20);
-		wc.put(w21.jvBanNianShu, w21);
-		System.err.println("欢迎来到世界杯查询软件");
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		for (;;) {
-			System.out.print("请输入:");
-			String input = br.readLine();
-			Wc_cs old = wc.get(input);
-			if (old == null) {
-				System.out.println("[没有举办世界杯]");
-				System.out.print("是否继续(y/n):");
-				String input1 = br.readLine();
-				if (input1.equals("y")) {
-					main(args);
-				} else {
-					System.err.print("已退出");
-					System.exit(0);
-				}
-			} else {
-				System.out.println("[冠军:" + old.guanJvn + "],[举办地点:" + old.jvBanDiDian + "]");
-			}
-		}
-	}
+    public static void main(String[] args) throws Exception {
+        // TODO Auto-generated method stub
+        Map<Integer, Wc_cs> wc = new HashMap<Integer, Wc_cs>();
+        Wc_cs w1 = new Wc_cs(1, 1930, "乌拉圭", "乌拉圭");
+        Wc_cs w2 = new Wc_cs(2, 1934, "意大利", "意大利");
+        Wc_cs w3 = new Wc_cs(3, 1938, "法国", "意大利");
+        Wc_cs w4 = new Wc_cs(4, 1950, "巴西", "乌拉圭");
+        Wc_cs w5 = new Wc_cs(5, 1954, "瑞士", "西德");
+        Wc_cs w6 = new Wc_cs(6, 1958, "瑞典", "巴西");
+        Wc_cs w7 = new Wc_cs(7, 1962, "智利", "巴西");
+        Wc_cs w8 = new Wc_cs(8, 1966, "英格兰", "英格兰");
+        Wc_cs w9 = new Wc_cs(9, 1970, "墨西哥", "巴西");
+        Wc_cs w10 = new Wc_cs(10, 1974, "前西德", "西德");
+        Wc_cs w11 = new Wc_cs(11, 1978, "阿根廷", "阿根廷");
+        Wc_cs w12 = new Wc_cs(12, 1982, "西班牙", "意大利");
+        Wc_cs w13 = new Wc_cs(13, 1986, "墨西哥", "阿根廷");
+        Wc_cs w14 = new Wc_cs(14, 1990, "意大利", "西德");
+        Wc_cs w15 = new Wc_cs(15, 1994, "美国", "巴西");
+        Wc_cs w16 = new Wc_cs(16, 1998, "法国", "法国");
+        Wc_cs w17 = new Wc_cs(17, 2002, "韩日", "巴西");
+        Wc_cs w18 = new Wc_cs(18, 2006, "德国", "意大利");
+        Wc_cs w19 = new Wc_cs(19, 2010, "南非", "西班牙");
+        Wc_cs w20 = new Wc_cs(20, 2014, "巴西", "德国");
+        Wc_cs w21 = new Wc_cs(21, 2018, "俄罗斯", "法国");
+        wc.put(w1.jvBanNianShu, w1);
+        wc.put(w2.jvBanNianShu, w2);
+        wc.put(w3.jvBanNianShu, w3);
+        wc.put(w4.jvBanNianShu, w4);
+        wc.put(w5.jvBanNianShu, w5);
+        wc.put(w6.jvBanNianShu, w6);
+        wc.put(w7.jvBanNianShu, w7);
+        wc.put(w8.jvBanNianShu, w8);
+        wc.put(w9.jvBanNianShu, w9);
+        wc.put(w10.jvBanNianShu, w10);
+        wc.put(w11.jvBanNianShu, w11);
+        wc.put(w12.jvBanNianShu, w12);
+        wc.put(w13.jvBanNianShu, w13);
+        wc.put(w14.jvBanNianShu, w14);
+        wc.put(w15.jvBanNianShu, w15);
+        wc.put(w16.jvBanNianShu, w16);
+        wc.put(w17.jvBanNianShu, w17);
+        wc.put(w18.jvBanNianShu, w18);
+        wc.put(w19.jvBanNianShu, w19);
+        wc.put(w20.jvBanNianShu, w20);
+        wc.put(w21.jvBanNianShu, w21);
+        System.err.println("欢迎来到世界杯查询软件");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for (;;) {
+            System.out.print("请输入:");
+            String input = br.readLine();
+            Wc_cs old = wc.get(input);
+            if (old == null) {
+                System.out.println("[没有举办世界杯]");
+                System.out.print("是否继续(y/n):");
+                String input1 = br.readLine();
+                if (input1.equals("y")) {
+                    main(args);
+                } else {
+                    System.err.print("已退出");
+                    System.exit(0);
+                }
+            } else {
+                System.out.println("[冠军:" + old.guanJvn + "],[举办地点:" + old.jvBanDiDian + "]");
+            }
+        }
+    }
 }
--- src/main/java/learning/java/practice/p20200614/Wc3.java
+++ src/main/java/learning/java/practice/p20200614/Wc3.java
@@ -16,28 +16,28 @@ import java.util.Set;
  */
 public class Wc3 {
     public static void main(String[] args) throws Exception {
-        Map<String, Wc_cs> wc = new HashMap<String, Wc_cs>();
-        Wc_cs w1 = new Wc_cs(1, "1930", "乌拉圭", "乌拉圭");
-        Wc_cs w2 = new Wc_cs(2, "1934", "意大利", "意大利");
-        Wc_cs w3 = new Wc_cs(3, "1938", "法国", "意大利");
-        Wc_cs w4 = new Wc_cs(4, "1950", "巴西", "乌拉圭");
-        Wc_cs w5 = new Wc_cs(5, "1954", "瑞士", "西德");
-        Wc_cs w6 = new Wc_cs(6, "1958", "瑞典", "巴西");
-        Wc_cs w7 = new Wc_cs(7, "1962", "智利", "巴西");
-        Wc_cs w8 = new Wc_cs(8, "1966", "英格兰", "英格兰");
-        Wc_cs w9 = new Wc_cs(9, "1970", "墨西哥", "巴西");
-        Wc_cs w10 = new Wc_cs(10, "1974", "前西德", "西德");
-        Wc_cs w11 = new Wc_cs(11, "1978", "阿根廷", "阿根廷");
-        Wc_cs w12 = new Wc_cs(12, "1982", "西班牙", "意大利");
-        Wc_cs w13 = new Wc_cs(13, "1986", "墨西哥", "阿根廷");
-        Wc_cs w14 = new Wc_cs(14, "1990", "意大利", "西德");
-        Wc_cs w15 = new Wc_cs(15, "1994", "美国", "巴西");
-        Wc_cs w16 = new Wc_cs(16, "1998", "法国", "法国");
-        Wc_cs w17 = new Wc_cs(17, "2002", "韩日", "巴西");
-        Wc_cs w18 = new Wc_cs(18, "2006", "德国", "意大利");
-        Wc_cs w19 = new Wc_cs(19, "2010", "南非", "西班牙");
-        Wc_cs w20 = new Wc_cs(20, "2014", "巴西", "德国");
-        Wc_cs w21 = new Wc_cs(21, "2018", "俄罗斯", "法国");
+        Map<Integer, Wc_cs> wc = new HashMap<Integer, Wc_cs>();
+        Wc_cs w1 = new Wc_cs(1, 1930, "乌拉圭", "乌拉圭");
+        Wc_cs w2 = new Wc_cs(2, 1934, "意大利", "意大利");
+        Wc_cs w3 = new Wc_cs(3, 1938, "法国", "意大利");
+        Wc_cs w4 = new Wc_cs(4, 1950, "巴西", "乌拉圭");
+        Wc_cs w5 = new Wc_cs(5, 1954, "瑞士", "西德");
+        Wc_cs w6 = new Wc_cs(6, 1958, "瑞典", "巴西");
+        Wc_cs w7 = new Wc_cs(7, 1962, "智利", "巴西");
+        Wc_cs w8 = new Wc_cs(8, 1966, "英格兰", "英格兰");
+        Wc_cs w9 = new Wc_cs(9, 1970, "墨西哥", "巴西");
+        Wc_cs w10 = new Wc_cs(10, 1974, "前西德", "西德");
+        Wc_cs w11 = new Wc_cs(11, 1978, "阿根廷", "阿根廷");
+        Wc_cs w12 = new Wc_cs(12, 1982, "西班牙", "意大利");
+        Wc_cs w13 = new Wc_cs(13, 1986, "墨西哥", "阿根廷");
+        Wc_cs w14 = new Wc_cs(14, 1990, "意大利", "西德");
+        Wc_cs w15 = new Wc_cs(15, 1994, "美国", "巴西");
+        Wc_cs w16 = new Wc_cs(16, 1998, "法国", "法国");
+        Wc_cs w17 = new Wc_cs(17, 2002, "韩日", "巴西");
+        Wc_cs w18 = new Wc_cs(18, 2006, "德国", "意大利");
+        Wc_cs w19 = new Wc_cs(19, 2010, "南非", "西班牙");
+        Wc_cs w20 = new Wc_cs(20, 2014, "巴西", "德国");
+        Wc_cs w21 = new Wc_cs(21, 2018, "俄罗斯", "法国");
         wc.put(w1.jvBanNianShu, w1);
         wc.put(w2.jvBanNianShu, w2);
         wc.put(w3.jvBanNianShu, w3);
@@ -59,8 +59,8 @@ public class Wc3 {
         wc.put(w19.jvBanNianShu, w19);
         wc.put(w20.jvBanNianShu, w20);
         wc.put(w21.jvBanNianShu, w21);
-        Set<Entry<String, Wc_cs>> entrys = wc.entrySet();
-        for (Entry<String, Wc_cs> entry : entrys) {
+        Set<Entry<Integer, Wc_cs>> entrys = wc.entrySet();
+        for (Entry<Integer, Wc_cs> entry : entrys) {
             if (entry.getValue().getJvBanDiDian().equals(entry.getValue().getGuanJvn())) {
                 System.out
                     .println(entry.getValue().getJieShu() + "," + entry.getValue().getJvBanNianShu()
