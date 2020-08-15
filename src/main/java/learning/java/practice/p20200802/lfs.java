41a6d3b4ddd1d97f8b684805291f77d902ace603
Xzai <Xzai@DESKTOP-FCHRPR4>
2020-08-15 17:20:43 +0800

    commit

--- src/main/java/learning/java/practice/p20200802/lfs.java
+++ src/main/java/learning/java/practice/p20200802/lfs.java
@@ -1,10 +1,15 @@
 package learning.java.practice.p20200802;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class lfs {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		ArrayList<lfs_> lfs = new ArrayList<lfs_>();
 		lfs_ m1 = new lfs_("张明", 35, "男");
 		lfs_ m2 = new lfs_("王芳", 28, "女");
@@ -22,7 +27,7 @@ public class lfs {
 			if (lfs__.nl >= 18 && lfs__.xb.equals("男")) {
 				sj = sj + 30;
 				q = q + 20;
- 			} else if (lfs__.nl >= 18 && lfs__.xb.equals("女")) {
+			} else if (lfs__.nl >= 18 && lfs__.xb.equals("女")) {
 				sj = sj + 65;
 				q = q + 45;
 			} else if (lfs__.nl < 18) {
@@ -30,8 +35,19 @@ public class lfs {
 				q = q + 20;
 			}
 		}
-		int xs = 0;
-		int sy = 0;
-		
+		System.out.println("理发师已经工作了" + sj + "分钟 , 挣了" + q + "元 , ta在"+getTime(sj)+"下班 .");
 	}
+	public static Date getTime(int mins) throws Exception {
+		DateFormat d = new SimpleDateFormat("yyyy年MM月dd日 hh:mm:ss");   //定义日期格式
+		Date date = d.parse("2020年08月10日 09:00:00");
+        //当前的时间
+        System.out.println(date);
+        //得到一个Calendar
+        Calendar calendar = Calendar.getInstance();
+        //设置当前时间
+        calendar.setTime(date);
+        //在当前时间上添加多少分钟
+        calendar.add(Calendar.MINUTE, mins);
+        return calendar.getTime();
+    }
 }
